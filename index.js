a61e819045481ac97454ba379b51fb35ae98eb5a
Kesha <kesha.marges@gmail.com>
2022-12-02 02:54:27 -0500

    commit tree b75e3181609e28d6fd30c88ece9f7f7ffe1e0942

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let charges;
+
+  if (feet <= 400) {
+    charges = 'This one is on me!';
+  } else if (400 < feet && feet <= 2000){
+    charges = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    charges = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    charges = 'No can do.';
+  }
+  return charges;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+    return (city === 'NYC') ? "Ok, sounds good." : "No go.";
+    }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  return (tip === 'generous') ? "Thank you so much."
+    :(tip === 'not as generous') ? "Thank you."
+    :  "Bye.";
+  }
